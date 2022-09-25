b452c3e0d646acaf75fe6f4ada4fc437f81fa335
NineR-9R <alexey3yt@gmail.com>
2022-09-25 14:23:43 +0300

    Update BlacklistedUsers.lua

--- BlacklistedUsers.lua
+++ BlacklistedUsers.lua
@@ -1,5 +1,24 @@
 
--- Do not comment UserIds, it blacklists too.
+--[[
 
+    Do not comment UserIds, it blacklists too.
+    Instead, place  _  symbol after first number.
 
-1_695750998, --Phantom, test
+    EXAMPLE OF COMMENT & UNBLACKLIST
+--0_123456789, --PlayerName (affiliation)
+
+--]]
+-------------------------------------------------
+
+--1_080973296, --xlISinner (RGK)
+--1_278913348, --0tcu (deemo & deemohook)
+--1_536581999, --c62y (deemo & deemohook)
+--116213795, --hitparts (deemo & deemohook)
+
+1133242788, --ccxmIcal (RGK)
+3033126474, --CTW038 (RGK)
+1097040221, --SYNFAT (skid)
+219183017, --Pawel12d (skid)
+209390, --Balie (sjors)
+83335331, --amuseren (cuteware)
+129062601, --ChinesePenguware (cuteware & other)
